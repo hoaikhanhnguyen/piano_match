e1ca2e7bd1a3a63864874bda10656a2913a797d4
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-10-04 14:50:01 -0700

    changed audio files to mp3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# piano_match
+# Piano Match
+
+# Match the music notes with the correct piano key
+
+# Uses Javascript and Jquery to build the keyboard and web page
+# Styling is done with SASS/SCSS and CSS bootstrap
--- js/keypress.js
+++ js/keypress.js
@@ -2,40 +2,40 @@ let allowed = false;
 $(window).keydown(function(e){
     if(allowed) {
         if (e.keyCode === 69) {
-            playSound(`piano_sounds/0${whiteKeys[0]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[0]}.mp3`);
             $("#white7").css("opacity", 1);
         } else if (e.keyCode === 82) {
-            playSound(`piano_sounds/0${whiteKeys[1]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[1]}.mp3`);
             $("#white8").css("opacity", 1);
         } else if (e.keyCode === 84) {
-            playSound(`piano_sounds/0${whiteKeys[2]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[2]}.mp3`);
             $("#white9").css("opacity", 1);
         } else if (e.keyCode === 89) {
-            playSound(`piano_sounds/0${whiteKeys[3]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[3]}.mp3`);
             $("#white10").css("opacity", 1);
         } else if (e.keyCode === 85) {
-            playSound(`piano_sounds/0${whiteKeys[4]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[4]}.mp3`);
             $("#white11").css("opacity", 1);
         } else if (e.keyCode === 73) {
-            playSound(`piano_sounds/0${whiteKeys[5]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[5]}.mp3`);
             $("#white12").css("opacity", 1);
         } else if (e.keyCode === 79) {
-            playSound(`piano_sounds/0${whiteKeys[6]}.wav`);
+            playSound(`piano_sounds/0${whiteKeys[6]}.mp3`);
             $("#white13").css("opacity", 1);
         } else if (e.keyCode === 52) {
-            playSound(`piano_sounds/0${blackKeys[0]}.wav`);
+            playSound(`piano_sounds/0${blackKeys[0]}.mp3`);
             $("#black8").addClass("black_key_select");
         } else if (e.keyCode === 53) {
-            playSound(`piano_sounds/0${blackKeys[1]}.wav`);
+            playSound(`piano_sounds/0${blackKeys[1]}.mp3`);
             $("#black9").addClass("black_key_select");
         } else if (e.keyCode === 55) {
-            playSound(`piano_sounds/0${blackKeys[3]}.wav`);
+            playSound(`piano_sounds/0${blackKeys[3]}.mp3`);
             $("#black11").addClass("black_key_select");
         }else if (e.keyCode === 56) {
-            playSound(`piano_sounds/0${blackKeys[4]}.wav`);
+            playSound(`piano_sounds/0${blackKeys[4]}.mp3`);
             $("#black12").addClass("black_key_select");
         }else if (e.keyCode === 57) {
-            playSound(`piano_sounds/0${blackKeys[5]}.wav`);
+            playSound(`piano_sounds/0${blackKeys[5]}.mp3`);
             $("#black13").addClass("black_key_select");
         }
     }
